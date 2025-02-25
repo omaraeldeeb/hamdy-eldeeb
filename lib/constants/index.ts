cc13d27f7af1a3ad3bdcc728d70df2ee3e408457
omaraeldeeb <omardaf421@gmail.com>
2025-02-25 19:47:23 +0200

    Initial Deployment

---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Hamdy ElDeeb";
+export const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION || "Hamdy ElDeep sanitaryware Store";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
