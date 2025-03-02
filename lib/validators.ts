7005b3db4c774c48089d5757c351489dc03f77c7
omaraeldeeb <omardaf421@gmail.com>
2025-03-03 01:13:24 +0200

    Implemented signup functionality and the signup form

--- lib/validators.ts
+++ lib/validators.ts
@@ -29,3 +29,18 @@ export const signInSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
+
+// Schema for signing user up
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters long"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
