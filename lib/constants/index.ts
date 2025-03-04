56acbe69e8453eb9643e3055d7431ef829444df1
omaraeldeeb <omardaf421@gmail.com>
2025-03-05 01:11:29 +0200

    updated app name

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Hamdy ElDeeb";
 export const APP_DESCRIPTION =
-  process.env.APP_DESCRIPTION || "Hamdy ElDeep sanitaryware Store";
+  process.env.APP_DESCRIPTION || "Hamdy ElDeeb sanitaryware Store";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
