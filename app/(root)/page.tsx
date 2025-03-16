a539280969ded18c492a840b3da7fb51f7447706
omaraeldeeb <omardaf421@gmail.com>
2025-03-17 00:01:34 +0200

    feat: Add product carousel, category drawer, and search functionality to enhance product browsing experience

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import ViewAllProductsButton from "@/components/view-all-products-button";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
 
 const homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      <ViewAllProductsButton />
     </>
   );
 };
---
+++ components/shared/product/product-carousel.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Product } from "@/types";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
+
+const ProductCarousel = ({ data }: { data: Product[] }) => {
+  const autoplayRef = useRef(
+    Autoplay({ delay: 4000, stopOnInteraction: true, stopOnMouseEnter: true })
+  );
+
+  return (
+    <Carousel
+      className="w-full mb-12"
+      opts={{ loop: true }}
+      plugins={[autoplayRef.current]}
+      onMouseEnter={() => autoplayRef.current.stop()} // Pause autoplay on hover
+      onMouseLeave={() => autoplayRef.current.play()} // Resume autoplay when hover ends
+    >
+      <CarouselContent>
+        {data.map((product: Product) => (
+          <CarouselItem key={product.id}>
+            <Link href={`/product/${product.slug}`}>
+              <div className="relative mx-auto">
+                <Image
+                  src={product.banner!}
+                  alt={product.name}
+                  height="0"
+                  width="0"
+                  sizes="100vw"
+                  className="w-full h-auto"
+                />
+                <div className="absolute inset-0 flex items-end justify-center">
+                  <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white">
+                    {product.name}
+                  </h2>
+                </div>
+              </div>
+            </Link>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
+
+export default ProductCarousel;
