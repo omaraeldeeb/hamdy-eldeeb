15b872bdddd1684a0b99bf7b953cff407911fdb0
omaraeldeeb <omardaf421@gmail.com>
2025-03-28 22:52:36 +0200

    fix: remove unused icon components from Calendar

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
@@ -53,10 +52,6 @@ function Calendar({
         ...classNames,
       }}
       captionLayout="dropdown"
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
-      }}
       {...props}
     />
   );
